df2a6dad0fa71e0e2e1d6e6c40f3e259ae1f258b
manulaPankaja <pankajabad1998@gmail.com>
2023-03-09 00:58:06 +0530

    A simple keeper app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,21 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes";
+
+
 
 function App(){
     return <div>
         <Header />
         <Footer />
-        <Note />
+        {notes.map(inside=>
+        <Note 
+            key={inside.key}
+            title={inside.title}
+            content={inside.content}
+        />
+    )}
     </div>
 }
 
